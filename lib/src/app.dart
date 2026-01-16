340000bda2dfefe8b308ded29bd72ea9b88f09a9
drawnskii <fernandobeltran.lab@gmail.com>
2026-01-16 15:11:53 -0500

    refactor(app): migrate MaterialApp to use GoRouter configuration

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nutrify/routes/app_routes.dart';
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(goRouterProvider);
+
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'Nutrify',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
       ),
+      routerConfig: router,
     );
   }
 }

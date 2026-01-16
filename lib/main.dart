049689a313cba1220253d7a7c64ed42031968cb8
drawnskii <fernandobeltran.lab@gmail.com>
2026-01-16 15:08:19 -0500

    refactor(main): initialize Firebase and Riverpod ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'src/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nutrify/src/app.dart';
+import 'package:nutrify/firebase_options.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    const ProviderScope(
+      child: MainApp()
+    ),
+  );
 }
